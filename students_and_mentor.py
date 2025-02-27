d3d5303452f54167a5bf64f0fa255e0cf473d667
wrk28 <andrei.wrk.28@gmail.com>
2025-02-28 02:02:51 +0300

    Add homework rating

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -1,27 +1,36 @@
 class Student:
     def __init__(self, name, surname, gender):
-        self.name = name
-        self.surname = surname
-        self.gender = gender
-        self.finished_courses = []
-        self.courses_in_progress = []
-        self.grades = {}
+        self.__name = name
+        self.__surname = surname
+        self.__gender = gender
+        self.__finished_courses = []
+        self.__courses_in_progress = []
+        self.__grades = {}
 
     def rate_lecturer(self, lecturer, course, point):
         pass
 
+    def receive_grade(self, course, grade):
+        if course in self.__courses_in_progress:
+            if course in self.__grades:
+                self.__grades[course] += [grade]
+            else:
+                self.__grades[course] = [grade]
+        else:
+            print('Ошибка!')
+
 
 class Mentor:
     def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
-        self.courses_attached = []
+        self.__name = name
+        self.__surname = surname
+        self._courses_attached = []
         
        
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
-        self.points = []
+        self.__points = {}
 
 
 class Reviewer(Mentor):
@@ -29,7 +38,14 @@ class Reviewer(Mentor):
         super().__init__(name, surname)
 
     def rate_hw(self, student, course, grade):
-        pass
+        if isinstance(student, Student) and course in self._courses_attached:
+            student.receive_grade(course, grade)
+        else:
+            print('Ошибка!')
+        
+
+
+           
 
 student1 = Student("Alex", "Johnson", "Male")
 lecture1 = Lecturer("John", "Johnson")
