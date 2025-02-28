781f96c85ef441cd95b6a6aefdf145f115b4fc33
wrk28 <andrei.wrk.28@gmail.com>
2025-02-28 15:00:59 +0300

    Update

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -14,12 +14,16 @@ class Student:
         else:
             print("Ошибка!")
 
+    def get_full_name(self):
+        return f'{self.__name} {self.__surname}'
+
     def receive_grade(self, course, grade):
         if course in self.__courses_in_progress:
             if course in self.__grades:
                 self.__grades[course] += [grade]
-            else:
-                self.__grades[course] = [grade]
+            # now the course is added by take_course method
+            # else:
+            #   self.__grades[course] = [grade]
         else:
             print('Ошибка!')
 
@@ -27,16 +31,21 @@ class Student:
         if  course not in self.__courses_in_progress and \
             course not in self.__finished_courses:
             self.__courses_in_progress.append(course)
+            self.__grades[course] = []
         else:
             print("Ошибка!")
 
-    def avg_grade(self):
-        adding = 0
-        number = 0  
-        for key, value in self.__grades.items():
-            adding += sum(value)
-            number += len(value)
-        return adding/number if number != 0 else None
+    def avg_grade(self, course=None):
+        if course is None:
+            courses = self.__grades.items()
+            grade_sum = sum(list(map(lambda course: sum(course[1]), courses)))
+            grade_count = sum(list(map(lambda course: len(course[1]), courses)))
+            return grade_sum/grade_count if grade_count > 0 else 0
+        elif course in self.__grades:
+            return sum(self.__grades[course]) / len(self.__grades[course]) \
+                if len(self.__grades[course]) > 0 else 0
+        else:
+            return 0
     
     def end_course(self, course):
         if  course in self.__courses_in_progress and \
@@ -94,6 +103,9 @@ class Mentor:
         self._surname = surname
         self._courses_attached = []
 
+    def get_full_name(self):
+        return f'{self._name} {self._surname}'
+
     def assigned_for_course(self, course):
         if  course not in self._courses_attached and \
             course not in self._courses_attached:
@@ -118,13 +130,18 @@ class Lecturer(Mentor):
     def get_course_list(self):
         return set(self._courses_attached)
     
-    def avg_point(self):
-        adding = 0
-        number = 0  
-        for key, value in self.__points.items():
-            adding += sum(value)
-            number += len(value)
-        return adding/number if number != 0 else None
+    def avg_point(self, course=None):
+        if course is None:
+            courses = self.__points.items()
+            point_sum = sum(list(map(lambda course: sum(course[1]), courses)))
+            point_count = sum(list(map(lambda course: len(course[1]), courses)))
+            return point_sum/point_count if point_count > 0 else 0
+        elif course in self.__points:
+            return sum(self.__points[course]) / len(self.__points[course]) \
+                if len(self.__points[course]) > 0 else 0
+        else:
+            return 0
+
     
     def __eq__(self, lecturer):
         if isinstance(lecturer, Lecturer):
@@ -182,23 +199,34 @@ class Reviewer(Mentor):
         return  f'Имя: {self._name}\n'\
                 f'Фамилия: {self._surname}'
     
-def average_grade_dict(students, course):
-    pass
+def average_grade_dict(students: Student, course):
+    grade_list = {}
+    for student in students:
+        grade_list[student.get_full_name()] = student.avg_grade(course)
+    return str(grade_list)
+
 
 def average_grade_total(students, course):
-    # adding = 0
-    # len = 0
-    # for student in students:
-    #     adding += student.avg_grade()
-    #     len += 1
-    # return adding/len if len !=0 else None
-    pass
+    adding = 0
+    len = 0
+    for student in students:
+        adding += student.avg_grade(course)
+        len += 1
+    return adding/len if len !=0 else None
 
 def average_point_dict(lectures, course):
-    pass
+    point_list = {}
+    for lecture in lectures:
+        point_list[lecture.get_full_name()] = lecture.avg_point(course)
+    return str(point_list)
 
 def average_point_total(lectures, course):
-    pass
+    adding = 0
+    len = 0
+    for lecture in lectures:
+        adding += lecture.avg_point(course)
+        len += 1
+    return adding/len if len !=0 else None
 
 def test():
     student1 = Student("Олег", "Естафьев", "Муж.")
@@ -211,6 +239,7 @@ def test():
 
     lecture1 = Lecturer("Борис", "Воскресенский")
     lecture1.assigned_for_course('математика')
+    lecture1.assigned_for_course('физика')
 
     lecture2 = Lecturer("Антон", "Бережный")
     lecture2.assigned_for_course('физика')
@@ -233,6 +262,8 @@ def test():
 
     student1.rate_lecturer(lecture1, 'математика', 6)
     student1.rate_lecturer(lecture1, 'математика', 7)
+    student1.rate_lecturer(lecture1, 'физика', 6)
+    student1.rate_lecturer(lecture1, 'физика', 7)
     student1.rate_lecturer(lecture2, 'физика', 8)
     student1.rate_lecturer(lecture2, 'физика', 8)
 
@@ -240,6 +271,8 @@ def test():
     student2.rate_lecturer(lecture1, 'математика', 7)
     student2.rate_lecturer(lecture2, 'физика', 8)
     student2.rate_lecturer(lecture2, 'физика', 8)
+    student2.rate_lecturer(lecture1, 'физика', 6)
+    student2.rate_lecturer(lecture1, 'физика', 7)
 
     student1.end_course('физика')
     student2.end_course('математика')
@@ -276,14 +309,14 @@ def test():
     lecture_list = [lecture1, lecture2]
     course = 'физика'
 
-    print(f'Средняя оценка по курсу {course}:',
+    print(f'Средняя оценка студентов по курсу {course}:',
           f'{average_grade_dict(student_list, course)}')
     print(f'Общая средняя оценка по курсу {course}:',
           f'{average_grade_total(student_list, course)}')
     print()
-    print(f'Средняя оценка по курсу {course}:',
+    print(f'Средняя оценка от студентов по лекциям {course}:',
           f'{average_point_dict(lecture_list, course)}')
-    print(f'Общая средняя оценка по курсу {course}:',
+    print(f'Общая средняя оценка от студентов по лекциям {course}:',
           f'{average_point_total(lecture_list, course)}')
 
 if __name__ == '__main__':
