8d992af10c9fb138ccf4e989970fb65e9a0a25a5
wrk28 <andrei.wrk.28@gmail.com>
2025-02-28 07:05:17 +0300

    Add output

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -8,7 +8,7 @@ class Student:
         self.__grades = {}
 
     def rate_lecturer(self, lecturer, course, point):
-        if  set(self.__courses_in_progress) & lecturer.getCourses() and\
+        if  set(self.__courses_in_progress) & lecturer.get_course_list() and\
             point in range(0, 11):
             lecturer.receive_points(course, point)   
         else:
@@ -23,21 +23,34 @@ class Student:
         else:
             print('Ошибка!')
 
+    def take_course(self, course):
+        if  course not in self.__courses_in_progress and \
+            course not in self.__finished_courses:
+            self.__courses_in_progress.append(course)
+        else:
+            print("Ошибка!")
+
     def __avg_grade(self):
-        grades = 0
+        adding = 0
         number = 0  
-        for course in self.__grades:
-            grades += sum(course.values())
-            number += len(course.values())
-        return grades/number if number != 0 else 0
+        for key, value in self.__grades.items():
+            adding += sum(value)
+            number += len(value)
+        return adding/number if number != 0 else None
+    
+    def end_course(self, course):
+        if  course in self.__courses_in_progress and \
+            course not in self.__finished_courses:
+            self.__courses_in_progress.remove(course)
+            self.__finished_courses.append(course)
 
 
     def __str__(self):
         return  f'Имя: {self.__name}\n'\
                 f'Фамилия: {self.__surname}\n'\
                 f'Средняя оценка за домашние задания: {self.__avg_grade()}\n'\
-                f'Курсы в процессе изучения: {",".join(self.__courses_in_progress)}\n'\
-                f'Завершенные курсы: {",".join(self.__finished_courses)}'
+                f'Курсы в процессе изучения: {", ".join(self.__courses_in_progress)}\n'\
+                f'Завершенные курсы: {", ".join(self.__finished_courses)}'
 
     def __eq__(self, student):
         if isinstance(student, Student):
@@ -80,7 +93,13 @@ class Mentor:
         self._name = name
         self._surname = surname
         self._courses_attached = []
-        
+
+    def assigned_for_course(self, course):
+        if  course not in self._courses_attached and \
+            course not in self._courses_attached:
+            self._courses_attached.append(course)
+        else:
+            print("Ошибка!")
        
 class Lecturer(Mentor):
     def __init__(self, name, surname):
@@ -89,23 +108,23 @@ class Lecturer(Mentor):
 
     def receive_points(self, course, point):
         if course in self._courses_attached:
-            if course in self.__points:
+            if course in self.__points.keys():
                 self.__points[course] += [point]
             else:
                 self.__points[course] = [point]
         else:
             print("Ошибка!")
 
-    def getCourses(self) -> set:
+    def get_course_list(self):
         return set(self._courses_attached)
     
     def __avg_point(self):
-        points = 0
+        adding = 0
         number = 0  
-        for course in self.__points:
-            points += sum(course.values())
-            number += len(course.values())
-        return points/number if number != 0 else 0
+        for key, value in self.__points.items():
+            adding += sum(value)
+            number += len(value)
+        return adding/number if number != 0 else None
     
     def __eq__(self, lecturer):
         if isinstance(lecturer, Lecturer):
@@ -162,13 +181,75 @@ class Reviewer(Mentor):
     def __str__(self):
         return  f'Имя: {self._name}\n'\
                 f'Фамилия: {self._surname}'
+    
+def average_grade_dict(students, course):
+    pass
 
+def average_grade_total(students, course):
+    pass
 
-student1 = Student("Alex", "Johnson", "Male")
-student2 = Student("Alexa", "Johnson", "Female")
-lecture1 = Lecturer("John", "Johnson")
-reviewer1 = Reviewer("Peter", "Johnson")
+def average_poin_dict(lectures, course):
+    pass
 
-print(student1 > student2)
-print(lecture1)
-print(reviewer1)
+def average_point_total(lectures, course):
+    pass
+
+def test():
+    student1 = Student("Олег", "Естафьев", "Муж.")
+    student1.take_course('физика')
+    student1.take_course('математика')
+
+    student2 = Student("Алёна", "Сергеевна", "Жен.")
+    student2.take_course('физика')
+    student2.take_course('математика')
+
+    lecture1 = Lecturer("Борис", "Воскресенский")
+    lecture1.assigned_for_course('математика')
+
+    lecture2 = Lecturer("Антон", "Бережный")
+    lecture2.assigned_for_course('физика')
+
+    reviewer1 = Reviewer("Пётр", "Алексеев")
+    reviewer1.assigned_for_course('физика')
+
+    reviewer2 = Reviewer("Роман", "Антонов")
+    reviewer2.assigned_for_course('математика')
+
+    reviewer1.rate_hw(student1, 'физика', 8)
+    reviewer1.rate_hw(student1, 'физика', 7)
+    reviewer1.rate_hw(student2, 'физика', 7)
+    reviewer1.rate_hw(student2, 'физика', 9)
+
+    reviewer2.rate_hw(student1, 'математика', 7)
+    reviewer2.rate_hw(student1, 'математика', 8)
+    reviewer2.rate_hw(student2, 'математика', 9)
+    reviewer2.rate_hw(student2, 'математика', 8)
+
+    student1.rate_lecturer(lecture1, 'математика', 6)
+    student1.rate_lecturer(lecture1, 'математика', 7)
+    student1.rate_lecturer(lecture2, 'физика', 8)
+    student1.rate_lecturer(lecture2, 'физика', 8)
+
+    student2.rate_lecturer(lecture1, 'математика', 6)
+    student2.rate_lecturer(lecture1, 'математика', 7)
+    student2.rate_lecturer(lecture2, 'физика', 8)
+    student2.rate_lecturer(lecture2, 'физика', 8)
+
+    student1.end_course('физика')
+    student2.end_course('математика')
+
+    print(student1)
+    print()
+    print(student2)
+    print()
+    print(lecture1)
+    print()
+    print(lecture2)
+    print()
+    print(reviewer1)
+    print()
+    print(reviewer2)
+    print()
+
+if __name__ == '__main__':
+    test()
